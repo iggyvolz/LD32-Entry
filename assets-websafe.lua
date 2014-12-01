78d082a6bdbceb0310f59fe6da0419e19c5da187
iggyvolz <iggyvolz@aim.com>
2014-12-01 15:28:31 +0000

    Make it websafe (???)

---
+++ assets-websafe.lua
@@ -0,0 +1,6 @@
+assets={
+    ["eijingle"] = love.audio.newSource("assets/eijingle.mp3","static"),
+    ["eilogo"] = love.graphics.newImage("assets/eilogo.png"),
+    ["love"] = love.graphics.newImage("assets/love.png"),
+    ["lua"] = love.graphics.newImage("assets/lua.png")
+}
