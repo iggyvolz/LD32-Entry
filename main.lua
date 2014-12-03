f6e605cb6e66933a9b7836f21d44738f96dcaedf
iggyvolz <iggyvolz@aim.com>
2014-12-03 18:23:57 +0000

    Add settings explanation

--- main.lua
+++ main.lua
@@ -4,6 +4,13 @@ assets.__index=assets
   SETTINGS:
     * boolean skipsplashes - Skips splashes if true
     * number volume - Sets volume; see https://www.love2d.org/wiki/love.audio.setVolume
+  Specify like: love . setting1=setting2 setting3[=true]
+  Not setting at runtime produces the default setting in config.lua
+  Specifying without an equals sign makes the setting equal to "true".
+  Please set a default value in config.lua if you intend to add a setting.
+  If you wish to make changes to the configuration settings without influencing the server default, you may wish to git update-index --assume-unchanged config.lua.
+  Please undo this with git update-index --no-assume-unchanged config.lua if you wish to commit changes on this file to the repo - remember to stash your local changes first.
+  If config.lua is updated and you are assuming it unchanged, you may want to do: git update-index --no-assume-unchanged config.lua && git stash && git pull origin master && git stash apply && git update-index --assume-unchanged config.lua.
 ]]
 settings={
 }
